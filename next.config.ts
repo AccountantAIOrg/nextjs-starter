180afafb89da5546fcb6edb7997d5a7b94967cea
Ashwani Kharwar <ashwanikumarkharwar83@gmail.com>
2026-04-23 18:15:04 +0530

    fix: fetch base path from env

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : "",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  },
 };
 
 export default nextConfig;
