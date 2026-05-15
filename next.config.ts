8e704141be4f1ff5be32fd042f7831ede5cde8a5
Ashwani Kharwar <ashwanikumarkharwar83@gmail.com>
2026-05-15 16:13:46 +0530

    remove base path config

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : "",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  },
 };
 
 export default nextConfig;
